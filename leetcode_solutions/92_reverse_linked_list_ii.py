d8d6a2afa8306bfe40ba37b57584129bfa495538
Vasyl_Boliukh <vskesha@gmail.com>
2023-09-07 23:10:11 +0300

    added 1 daily task to 'leetcode_solutions'

---
+++ leetcode_solutions/92_reverse_linked_list_ii.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if left == right:
+            return head
+
+        curr = dummy = ListNode(0, head)
+
+        for i in range(left - 1):
+            curr = curr.next
+
+        end1, end2 = curr, curr.next
+        prev, curr = end2, end2.next
+
+        for i in range(right - left):
+            curr.next, prev, curr = prev, curr, curr.next
+
+        end1.next = prev
+        end2.next = curr
+
+        return dummy.next
+
+
+def test():
+    sol = Solution()
+
+
+if __name__ == '__main__':
+    test()
