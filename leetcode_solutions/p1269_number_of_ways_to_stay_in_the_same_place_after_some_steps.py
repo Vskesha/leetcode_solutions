297856ff817ea5acc1259afbd13af13fb71fb0f2
Vskesha <vskesha@gmail.com>
2023-10-17 22:09:35 +0300

    added 2 daily task to leetcode_solutions

---
+++ leetcode_solutions/p1269_number_of_ways_to_stay_in_the_same_place_after_some_steps.py
@@ -0,0 +1,36 @@
+from functools import lru_cache
+
+
+class Solution:
+    def numWays(self, steps: int, arrLen: int) -> int:
+        mod = 1_000_000_007
+
+        @lru_cache(None)
+        def dp(i, rem):
+            if i < 0 or i >= arrLen or rem < i:
+                return 0
+            if rem == i:
+                return 1
+            return (dp(i, rem - 1) + dp(i - 1, rem - 1) + dp(i + 1, rem - 1)) % mod
+
+        return dp(0, steps)
+
+
+def test():
+    sol = Solution()
+
+    print('Test 1 ... ', end='')
+    assert sol.numWays(steps=3, arrLen=2) == 4
+    print('ok')
+
+    print('Test 2 ... ', end='')
+    assert sol.numWays(steps=2, arrLen=4) == 2
+    print('ok')
+
+    print('Test 3 ... ', end='')
+    assert sol.numWays(steps=4, arrLen=2) == 8
+    print('ok')
+
+
+if __name__ == '__main__':
+    test()
---
+++ leetcode_solutions/p1361_validate_binary_tree_nodes.py
@@ -0,0 +1,114 @@
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
+        income = [0] * n
+        for i in range(n):
+            for val in (leftChild[i], rightChild[i]):
+                if val != -1:
+                    if not income[val]:
+                        income[val] = 1
+                    else:
+                        return False
+
+        roots = []
+        for i, k in enumerate(income):
+            if not k:
+                if not roots:
+                    roots.append(i)
+                else:
+                    return False
+
+        i = 0
+        while i < len(roots):
+            curr = roots[i]
+            if leftChild[curr] != -1:
+                roots.append(leftChild[curr])
+            if rightChild[curr] != -1:
+                roots.append(rightChild[curr])
+            i += 1
+
+        return len(roots) == n
+
+
+class Solution1:
+    def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
+        income = [0] * (n + 1)
+        for a, b in zip(leftChild, rightChild):
+            income[a] += 1
+            income[b] += 1
+        income.pop()
+
+        roots = []
+        for i, k in enumerate(income):
+            if not k:
+                if not roots:
+                    roots.append(i)
+                else:
+                    return False
+            if k > 1:
+                return False
+
+        i = 0
+        while i < len(roots):
+            curr = roots[i]
+            if leftChild[curr] != -1:
+                roots.append(leftChild[curr])
+            if rightChild[curr] != -1:
+                roots.append(rightChild[curr])
+            i += 1
+
+        return len(roots) == n
+
+
+class Solution2:
+    def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
+        income = [0] * (n + 1)
+        for a, b in zip(leftChild, rightChild):
+            income[a] += 1
+            income[b] += 1
+        income.pop()
+
+        roots = []
+        for i, k in enumerate(income):
+            if not k:
+                if not roots:
+                    roots.append(i)
+                else:
+                    return False
+            if k > 1:
+                return False
+
+        bfs = deque(roots)
+        visited = set()
+        while bfs:
+            curr = bfs.popleft()
+            visited.add(curr)
+            if leftChild[curr] != -1:
+                bfs.append(leftChild[curr])
+            if rightChild[curr] != -1:
+                bfs.append(rightChild[curr])
+
+        return len(visited) == n
+
+
+def test():
+    sol = Solution()
+
+    print('Test 1 ... ', end='')
+    assert sol.validateBinaryTreeNodes(n=4, leftChild=[1, -1, 3, -1], rightChild=[2, -1, -1, -1]) == True
+    print('ok')
+
+    print('Test 2 ... ', end='')
+    assert sol.validateBinaryTreeNodes(n=4, leftChild=[1, -1, 3, -1], rightChild=[2, 3, -1, -1]) == False
+    print('ok')
+
+    print('Test 3 ... ', end='')
+    assert sol.validateBinaryTreeNodes(n=2, leftChild=[1, 0], rightChild=[-1, -1]) == False
+    print('ok')
+
+
+if __name__ == '__main__':
+    test()
