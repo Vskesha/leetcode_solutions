65f1dbdf656ebb3fd328cc05cc19787f49925d90
Vskesha <vskesha@gmail.com>
2023-12-02 17:45:07 +0200

    added 1 solution of daily tasks

--- leetcode_solutions/p1160_find_words_that_can_be_formed_by_characters.py
+++ leetcode_solutions/p1160_find_words_that_can_be_formed_by_characters.py
@@ -1,8 +1,9 @@
 from collections import Counter
+from typing import List
 
 
 class Solution:
-    def countCharacters(self, words: list[str], chars: str) -> int:
+    def countCharacters(self, words: List[str], chars: str) -> int:
         ans = 0
         counter = Counter(chars)
 
@@ -16,11 +17,26 @@ class Solution:
         return ans
 
 
+class Solution2:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        chars = Counter(chars)
+        ans = 0
+        for word in words:
+            for ch, n in Counter(word).items():
+                if chars[ch] < n:
+                    break
+            else:
+                ans += len(word)
+        return ans
+
+
 def main():
     sol = Solution()
+
     print('Test 1 ... ', end='')
     assert sol.countCharacters(words=["cat", "bt", "hat", "tree"], chars="atach") == 6
     print('ok')
+
     print('Test 2 ... ', end='')
     assert sol.countCharacters(words=["hello", "world", "leetcode"], chars="welldonehoneyr") == 10
     print('ok')
