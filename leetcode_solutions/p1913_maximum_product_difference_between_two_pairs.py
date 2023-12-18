91ed6029992150f0fdad5a6d46dad3bc3e2df9a3
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2023-12-18 09:58:58 +0200

    added 1 daily solution

---
+++ leetcode_solutions/p1913_maximum_product_difference_between_two_pairs.py
@@ -0,0 +1,46 @@
+from heapq import nlargest, nsmallest
+from operator import mul
+from typing import List
+
+
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        return mul(*nlargest(2, nums)) - mul(*nsmallest(2, nums))
+
+
+class Solution1:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        nums.sort()
+        return nums[-1] * nums[-2] - nums[0] * nums[1]
+
+
+class Solution2:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        s, ss, sb, b = sorted(nums[:4])
+        for i in range(4, len(nums)):
+            n = nums[i]
+            if n > b:
+                b, sb = n, b
+            elif n > sb:
+                sb = n
+            elif n < s:
+                s, ss = n, s
+            elif n < ss:
+                ss = n
+        return b * sb - s * ss
+
+
+def test():
+    sol = Solution()
+
+    print('Test 1... ', end='')
+    assert sol.maxProductDifference(nums=[5, 6, 2, 7, 4]) == 34
+    print('OK')
+
+    print('Test 2... ', end='')
+    assert sol.maxProductDifference(nums=[4, 2, 5, 9, 7, 4, 8]) == 64
+    print('OK')
+
+
+if __name__ == '__main__':
+    test()
