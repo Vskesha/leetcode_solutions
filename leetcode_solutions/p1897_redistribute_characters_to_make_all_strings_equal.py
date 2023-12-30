edcad9a88afbacc4a405a8d779d10232e6f12cb4
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2023-12-30 12:10:44 +0200

    added 2 daily solutions

---
+++ leetcode_solutions/p1897_redistribute_characters_to_make_all_strings_equal.py
@@ -0,0 +1,37 @@
+from collections import Counter
+from itertools import chain
+from typing import List
+
+
+class Solution:
+    def makeEqual(self, words: List[str]) -> bool:
+        lw = len(words)
+        wds = "".join(words)
+
+        if len(wds) % lw:
+            return False
+        for n in Counter(wds).values():
+            if n % lw:
+                return False
+        return True
+
+
+class Solution2:
+    def makeEqual(self, words: List[str]) -> bool:
+        return all(c % len(words) == 0 for c in Counter(chain(*words)).values())
+
+
+def test():
+    sol = Solution()
+
+    print("Test 1... ", end="")
+    assert sol.makeEqual(words=["abc", "aabc", "bc"]) is True
+    print("OK")
+
+    print("Test 2... ", end="")
+    assert sol.makeEqual(words=["ab", "a"]) is False
+    print("OK")
+
+
+if __name__ == "__main__":
+    test()
---
+++ leetcode_solutions/p946_validate_stack_sequences.py
@@ -0,0 +1,88 @@
+from typing import List
+
+
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack = []
+        i = 0
+        for n in popped:
+            if n in stack:
+                if stack[-1] == n:
+                    stack.pop()
+                    continue
+                else:
+                    return False
+            while pushed[i] != n:
+                stack.append(pushed[i])
+                i += 1
+            i += 1
+        return True
+
+
+class Solution1:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack = []
+        lp = len(pushed)
+        i = 0
+        for n in popped:
+            if stack and stack[-1] == n:
+                stack.pop()
+                continue
+            if i >= lp:
+                return False
+            while i < lp and pushed[i] != n:
+                stack.append(pushed[i])
+                i += 1
+            i += 1
+        return True
+
+
+class Solution2:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack = []
+        lp = len(pushed)
+        i = j = 0
+        while i <= lp:
+            while stack and stack[-1] == popped[j]:
+                stack.pop()
+                j += 1
+            while i < lp and pushed[i] != popped[j]:
+                stack.append(pushed[i])
+                i += 1
+            i += 1
+            j += 1
+        return not stack
+
+
+class Solution3:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack = []
+        j = 0
+        for x in pushed:
+            stack.append(x)
+            while stack and stack[-1] == popped[j]:
+                stack.pop()
+                j += 1
+        return stack == []
+
+
+def test():
+    sol = Solution()
+
+    print("Test 1... ", end="")
+    assert (
+        sol.validateStackSequences(pushed=[1, 2, 3, 4, 5], popped=[4, 5, 3, 2, 1])
+        is True
+    )
+    print("OK")
+
+    print("Test 2... ", end="")
+    assert (
+        sol.validateStackSequences(pushed=[1, 2, 3, 4, 5], popped=[4, 3, 5, 1, 2])
+        is False
+    )
+    print("OK")
+
+
+if __name__ == "__main__":
+    test()
