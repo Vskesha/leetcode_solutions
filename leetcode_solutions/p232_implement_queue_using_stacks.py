f974f85e83b3d3f029f810e8372dbe3625a3e01b
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-01-29 17:18:14 +0200

    added 1 daily solution

--- leetcode_solutions/p232_implement_queue_using_stacks.py
+++ leetcode_solutions/p232_implement_queue_using_stacks.py
@@ -1,5 +1,50 @@
 class MyQueue:
+    def __init__(self):
+        self.inp = []
+        self.out = []
 
+    def push(self, x: int) -> None:
+        self.inp.append(x)
+
+    def pop(self) -> int:
+        if not self.out:
+            self.move()
+        return self.out.pop()
+
+    def peek(self) -> int:
+        if not self.out:
+            self.move()
+        return self.out[-1]
+
+    def empty(self) -> bool:
+        return not (self.inp or self.out)
+
+    def move(self) -> None:
+        while self.inp:
+            self.out.append(self.inp.pop())
+
+
+class MyQueue1:
+    def __init__(self):
+        self.st = []
+        self.aux = []
+
+    def push(self, x: int) -> None:
+        [self.aux.append(self.st.pop()) for _ in range(len(self.st))]
+        self.st.append(x)
+        [self.st.append(self.aux.pop()) for _ in range(len(self.aux))]
+
+    def pop(self) -> int:
+        return self.st.pop()
+
+    def peek(self) -> int:
+        return self.st[-1]
+
+    def empty(self) -> bool:
+        return len(self.st) == 0
+
+
+class MyQueue2:
     def __init__(self):
         self.main = []
         self.aux = []
@@ -26,9 +71,31 @@ class MyQueue:
     def empty(self) -> bool:
         return not self.main
 
+
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
 # obj.push(x)
 # param_2 = obj.pop()
 # param_3 = obj.peek()
 # param_4 = obj.empty()
+
+
+def test():
+    null = None
+    false = False
+    true = True
+
+    commands = ["MyQueue", "push", "push", "peek", "pop", "empty"]
+    args = [[], [1], [2], [], [], []]
+    outputs = [null, null, null, 1, 1, false]
+
+    queue = MyQueue()
+    for i in range(1, len(commands)):
+        print(f"Test {i}... ", end="")
+        result = getattr(queue, commands[i])(*args[i])
+        assert result == outputs[i]
+        print("OK")
+
+
+if __name__ == "__main__":
+    test()
