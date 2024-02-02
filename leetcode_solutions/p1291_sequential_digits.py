49cf4e04713c731a8177a637a26890016fdab9b3
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-02-02 21:40:11 +0200

    added 1 daily solution

---
+++ leetcode_solutions/p1291_sequential_digits.py
@@ -0,0 +1,39 @@
+from bisect import bisect_left, bisect_right
+from typing import List
+
+
+class Solution:
+    tmp = "123456789"
+    nums = []
+    for l in range(1, 10):
+        for st in range(10 - l):
+            nums.append(int(tmp[st : st + l]))
+
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        fr = bisect_left(Solution.nums, low)
+        to = bisect_right(Solution.nums, high)
+        return Solution.nums[fr:to]
+
+
+def test():
+    sol = Solution()
+
+    print("Test 1... ", end="")
+    assert sol.sequentialDigits(low=100, high=300) == [123, 234]
+    print("OK")
+
+    print("Test 2... ", end="")
+    assert sol.sequentialDigits(low=1000, high=13000) == [
+        1234,
+        2345,
+        3456,
+        4567,
+        5678,
+        6789,
+        12345,
+    ]
+    print("OK")
+
+
+if __name__ == "__main__":
+    test()
