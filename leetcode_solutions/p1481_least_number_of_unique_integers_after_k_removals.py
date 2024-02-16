ece22a2c1fb66d42ee3ddc3ec5e355641b4d30e9
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-02-16 14:23:03 +0200

    added 1 daily solution

---
+++ leetcode_solutions/p1481_least_number_of_unique_integers_after_k_removals.py
@@ -0,0 +1,37 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        vals = sorted(Counter(arr).values())
+        for i, n in enumerate(vals):
+            if n > k:
+                return len(vals) - i
+            else:
+                k -= n
+        return 0
+
+
+class Solution2:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        vals = sorted(Counter(arr).values(), reverse=True)
+        while vals and vals[-1] <= k:
+            k -= vals.pop()
+        return len(vals)
+
+
+def test():
+    sol = Solution()
+
+    print("Test 1... ", end="")
+    assert sol.findLeastNumOfUniqueInts(arr=[5, 5, 4], k=1) == 1
+    print("OK")
+
+    print("Test 2... ", end="")
+    assert sol.findLeastNumOfUniqueInts(arr=[4, 3, 1, 1, 3, 3, 2], k=3) == 2
+    print("OK")
+
+
+if __name__ == "__main__":
+    test()
