29f3c30aad6e54024a083a84a98c0b3e187e1e37
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-02-21 23:59:56 +0200

    added 1 daily solution

---
+++ leetcode_solutions/p201_bitwise_and_of_numbers_range.py
@@ -0,0 +1,44 @@
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        shift = 0
+        while left < right:
+            shift += 1
+            left >>= 1
+            right >>= 1
+        return left << shift
+
+
+class Solution2:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        l = bin(left)[2:].zfill(32)
+        r = bin(right)[2:].zfill(32)
+
+        ans = ""
+        for a, b in zip(l, r):
+            if a == b:
+                ans += a
+            else:
+                break
+
+        ans += "0" * (32 - len(ans))
+        return int(ans, 2)
+
+
+def test():
+    sol = Solution()
+
+    print("Test 1... ", end="")
+    assert sol.rangeBitwiseAnd(left=5, right=7) == 4
+    print("OK")
+
+    print("Test 2... ", end="")
+    assert sol.rangeBitwiseAnd(left=0, right=0) == 0
+    print("OK")
+
+    print("Test 3... ", end="")
+    assert sol.rangeBitwiseAnd(left=1, right=2147483647) == 0
+    print("OK")
+
+
+if __name__ == "__main__":
+    test()
