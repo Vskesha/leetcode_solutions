d8f834a31176b4a4bcdcef7f9fb631ad630d7de3
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-03-06 14:52:35 +0200

    modified 1 daily solution

--- leetcode_solutions/p141_linked_list_cycle.py
+++ leetcode_solutions/p141_linked_list_cycle.py
@@ -1,4 +1,5 @@
-from typing import Optional
+from functools import wraps
+from typing import Optional, List
 
 
 # Definition for singly-linked list.
@@ -6,6 +7,33 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+
+
+def sol_decorator(cls):
+    @wraps(cls, updated=())
+    class Wrapper:
+        def __init__(self, *args, **kwargs):
+            self.original = cls(*args, **kwargs)
+
+        def hasCycle(self, head: Optional[List], pos: int) -> bool:
+            head = self.arr2linkedlist(head, pos)
+            return self.original.hasCycle(head)
+
+        def arr2linkedlist(self, arr: Optional[List], pos: int) -> Optional[ListNode]:
+            if not arr:
+                return None
+
+            dummy = node = ListNode(None)
+            cycle = None
+            for i, x in enumerate(arr):
+                node.next = ListNode(x)
+                node = node.next
+                if i == pos:
+                    cycle = node
+            node.next = cycle
+            return dummy.next
+
+    return Wrapper
 
 
 class Solution:
@@ -22,7 +50,7 @@ class Solution:
 class Solution1:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         while head:
-            if hasattr(head, 'pos'):
+            if hasattr(head, "pos"):
                 return True
             head.pos = 0
             head = head.next
@@ -39,3 +67,23 @@ class Solution2:
             visited.add(head)
             head = head.next
         return False
+
+
+def test_has_cycle():
+    sol = sol_decorator(Solution)()
+
+    print("Test 1... ", end="")
+    assert sol.hasCycle(head=[3, 2, 0, -4], pos=1) is True
+    print("OK")
+
+    print("Test 2... ", end="")
+    assert sol.hasCycle(head=[1, 2], pos=0) is True
+    print("OK")
+
+    print("Test 3... ", end="")
+    assert sol.hasCycle(head=[1], pos=-1) is False
+    print("OK")
+
+
+if __name__ == "__main__":
+    test_has_cycle()
