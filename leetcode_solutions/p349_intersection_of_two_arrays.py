23b5d045bf69926a6deecdcd2435f1c0cf7ff3ce
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-03-11 11:52:03 +0200

    added 1 daily solution and 2 modified

---
+++ leetcode_solutions/p349_intersection_of_two_arrays.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return list(set(nums1) & set(nums2))
+
+
+def test_intersection():
+    sol = Solution()
+
+    print("Test 1... ", end="")
+    assert set(sol.intersection(nums1=[1, 2, 2, 1], nums2=[2, 2])) == {2}
+    print("OK")
+
+    print("Test 2... ", end="")
+    assert set(sol.intersection(nums1=[4, 9, 5], nums2=[9, 4, 9, 8, 4])) == {9, 4}
+    print("OK")
+
+
+if __name__ == "__main__":
+    test_intersection()
--- leetcode_solutions/p791_custom_sort_string.py
+++ leetcode_solutions/p791_custom_sort_string.py
@@ -1,16 +1,58 @@
+from collections import Counter
+
+
 class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        order_dict = {ch: i for i, ch in enumerate(order)}
+        return "".join(sorted(s, key=lambda ch: order_dict.get(ch, 30)))
+
+
+class Solution1:
+    def customSortString(self, order: str, s: str) -> str:
+        cnt = Counter(s)
+        ans = ""
+        for ch in order:
+            ans += ch * cnt[ch]
+            del cnt[ch]
+        for ch in cnt:
+            ans += ch * cnt[ch]
+        return ans
+
+
+class Solution2:
     def customSortString(self, order: str, s: str) -> str:
         ids = {c: i for i, c in enumerate(order)}
         lst = list(s)
         lst.sort(key=lambda c: ids.get(c, -1))
-        return ''.join(lst)
+        return "".join(lst)
 
 
-def main():
+def verify_sequence(sorted_str: str, order: str) -> bool:
+    common = set(sorted_str) & set(order)
+    sorted_list = []
+    for c in sorted_str:
+        if c not in sorted_list and c in common:
+            sorted_list.append(c)
+    order_list = [c for c in order if c in common]
+    return sorted_list == order_list
+
+
+def test_custom_sort_string():
     sol = Solution()
-    print('cbad ===', sol.customSortString(order="cba", s="abcd"))
-    print('cbad ===', sol.customSortString(order="cbafg", s="abcd"))
+
+    print("Test 1... ", end="")
+    assert verify_sequence(
+        sorted_str=sol.customSortString(order="cba", s="abcd"), order="cba"
+    )
+    print("OK")
+
+    print("Test 2... ", end="")
+    assert verify_sequence(
+        sorted_str=sol.customSortString(order="bcafg", s="abcd"),
+        order="bcafg",
+    )
+    print("OK")
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    test_custom_sort_string()
