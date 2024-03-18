83ec5a4a650e76a46361704f395307f602e53398
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-03-18 16:03:55 +0200

    added 1 daily solution

---
+++ leetcode_solutions/p452_minimum_number_of_arrows_to_burst_balloons.py
@@ -0,0 +1,48 @@
+from math import inf
+from typing import List
+
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points.sort(key=lambda x: x[1])
+        end = float("-inf")
+        ans = 0
+        for p in points:
+            if p[0] > end:
+                ans += 1
+                end = p[1]
+        return ans
+
+
+class Solution2:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points.sort()
+        end = -inf
+        ans = 0
+        for xs, xe in points:
+            if xs > end:
+                ans += 1
+                end = xe
+            else:
+                end = min(end, xe)
+        return ans
+
+
+def test_find_min_arrow_shots():
+    sol = Solution()
+
+    print("Test 1... ", end="")
+    assert sol.findMinArrowShots(points=[[10, 16], [2, 8], [1, 6], [7, 12]]) == 2
+    print("OK")
+
+    print("Test 2... ", end="")
+    assert sol.findMinArrowShots(points=[[1, 2], [3, 4], [5, 6], [7, 8]]) == 4
+    print("OK")
+
+    print("Test 3... ", end="")
+    assert sol.findMinArrowShots(points=[[1, 2], [2, 3], [3, 4], [4, 5]]) == 2
+    print("OK")
+
+
+if __name__ == "__main__":
+    test_find_min_arrow_shots()
