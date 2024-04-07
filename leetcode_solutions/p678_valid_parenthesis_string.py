b4fdbefe464ef86cecc77b9f901fc3e6859cabff
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-04-07 17:40:37 +0300

    added 1 daily solution

---
+++ leetcode_solutions/p678_valid_parenthesis_string.py
@@ -0,0 +1,73 @@
+from collections import deque
+
+
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        opened = []
+        asters = deque()
+
+        for i, ch in enumerate(s):
+            if ch == "(":
+                opened.append(i)
+            elif ch == "*":
+                asters.append(i)
+            elif opened:
+                opened.pop()
+            elif asters:
+                asters.popleft()
+            else:
+                return False
+
+        while opened and asters and opened[-1] < asters[-1]:
+            opened.pop()
+            asters.pop()
+
+        return not opened
+
+
+class Solution2:
+    def checkValidString(self, s: str) -> bool:
+        lo, hi = 0, 0
+
+        for c in s:
+            if c == "(":
+                lo, hi = lo + 1, hi + 1
+            elif c == ")":
+                lo, hi = lo - 1, hi - 1
+            else:
+                lo = lo - 1
+                hi = hi + 1
+            if hi < 0:
+                return False
+            if lo < 0:
+                lo = 0
+        return lo == 0
+
+
+def test_check_valid_string():
+    sol = Solution()
+
+    print("Test 1... ", end="")
+    assert sol.checkValidString(s="()") is True
+    print("OK")
+
+    print("Test 2... ", end="")
+    assert sol.checkValidString(s="(*)") is True
+    print("OK")
+
+    print("Test 3... ", end="")
+    assert sol.checkValidString(s="(*))") is True
+    print("OK")
+
+    print("Test 4... ", end="")
+    assert (
+        sol.checkValidString(
+            s="(((((*(()((((*((**(((()()*)()()()*((((**)())*)*)))))))(())(()))())((*()()(((()((()*(())*(()**)()(())"
+        )
+        is False
+    )
+    print("OK")
+
+
+if __name__ == "__main__":
+    test_check_valid_string()
