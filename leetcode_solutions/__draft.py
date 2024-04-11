02f09ab2911d833dbabf80b5be929597f44df003
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-04-11 15:33:35 +0300

    modified 1 daily solution

--- leetcode_solutions/__draft.py
+++ leetcode_solutions/__draft.py
@@ -1,10 +1,1 @@
-error = Exception("Some error occurred.")
-print(type(error))
-answer = {
-    "message": "Message about error",
-    "detail": "Error occurred" + str(error),
-    "deep_info": f"{error}",
-}
-print(answer.get("message"))
-print(answer.get("detail"))
-print(answer.get("deep_info"))
+print(dir({1: 1}))
--- leetcode_solutions/p402_remove_k_digits.py
+++ leetcode_solutions/p402_remove_k_digits.py
@@ -3,41 +3,55 @@ from functools import lru_cache
 
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
+        if k >= len(num):
+            return "0"
+
+        stack = []
+        for n in num:
+            while k and stack and n < stack[-1]:
+                stack.pop()
+                k -= 1
+            stack.append(n)
+        for _ in range(k):
+            stack.pop()
+        return "".join(stack).lstrip("0") or "0"
+
+
+class Solution1:
+    def removeKdigits(self, num: str, k: int) -> str:
         st = list()
         for n in num:
             while st and k and st[-1] > n:
                 st.pop()
                 k -= 1
 
-            if st or n != '0':  # prevent leading zeros
+            if st or n != "0":  # prevent leading zeros
                 st.append(n)
 
         if k:  # not fully spent
             st = st[0:-k]
 
-        return ''.join(st) or '0'
+        return "".join(st) or "0"
 
 
 class Solution2:
     def removeKdigits(self, num: str, k: int) -> str:
-
         @lru_cache
         def dp(num, k):
-
             if k == 0:
                 return num
 
             if k == len(num):
-                return ''
+                return ""
 
             a = num[0] + dp(num[1:], k)
             b = dp(num[1:], k - 1)
 
             return a if int(a) < int(b) else b
 
-        n = dp(num, k).lstrip('0')
+        n = dp(num, k).lstrip("0")
 
-        return n if n else '0'
+        return n if n else "0"
 
 
 class Solution3:
@@ -45,11 +59,10 @@ class Solution3:
         res = []
         st = 0
         li = 0  # last_investigated
-        m = ''
+        m = ""
         mp = {str(i): 0 for i in range(10)}  # digit counter
 
         while k > 0:
-
             if st + k >= len(num):
                 st = len(num)
                 break
@@ -75,27 +88,27 @@ class Solution3:
             st += 1
 
         res += num[st:]
-        res = ''.join(res)
-        res = res.lstrip('0')
+        res = "".join(res)
+        res = res.lstrip("0")
 
-        return res if res else '0'
+        return res if res else "0"
 
 
 def test():
     sol = Solution()
 
-    print('Test 1... ', end='')
-    assert sol.removeKdigits(num="1432219", k=3) == '1219'
-    print('OK')
+    print("Test 1... ", end="")
+    assert sol.removeKdigits(num="1432219", k=3) == "1219"
+    print("OK")
 
-    print('Test 2... ', end='')
-    assert sol.removeKdigits(num="10200", k=1) == '200'
-    print('OK')
+    print("Test 2... ", end="")
+    assert sol.removeKdigits(num="10200", k=1) == "200"
+    print("OK")
 
-    print('Test 3... ', end='')
-    assert sol.removeKdigits(num="10", k=2) == '0'
-    print('OK')
+    print("Test 3... ", end="")
+    assert sol.removeKdigits(num="10", k=2) == "0"
+    print("OK")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test()
