901f5fc02baf02074288cb49441822813b955f90
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-05-12 00:12:43 +0300

    modified 4 daily solutions of mock assessments

---
+++ leetcode_solutions/p27_remove_element.py
@@ -0,0 +1,51 @@
+from typing import List
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        ln = len(nums)
+        l, r = 0, ln - 1
+
+        while l <= r:
+            if nums[l] == val:
+                nums[l], nums[r] = nums[r], nums[l]
+                r -= 1
+                ln -= 1
+            else:
+                l += 1
+
+        return ln
+
+
+class Solution2:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+
+        for n in nums:
+            if n != val:
+                nums[k] = n
+                k += 1
+
+        return k
+
+
+def test_remove_element():
+    sol = Solution()
+
+    print("Test 1... ", end="")
+    nums = [3, 2, 2, 3]
+    assert sol.removeElement(nums=nums, val=3) == 2
+    nums_v = [2, 2]
+    assert set(nums_v) == set(nums[: len(nums_v)])
+    print("OK")
+
+    print("Test 2... ", end="")
+    nums = [0, 1, 2, 2, 3, 0, 4, 2]
+    assert sol.removeElement(nums=nums, val=2) == 5
+    nums_v = [0, 1, 4, 0, 3]
+    assert set(nums_v) == set(nums[: len(nums_v)])
+    print("OK")
+
+
+if __name__ == "__main__":
+    test_remove_element()
--- leetcode_solutions/p39_combination_sum.py
+++ leetcode_solutions/p39_combination_sum.py
@@ -3,6 +3,28 @@ from typing import List
 
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        res = []
+        lc = len(candidates)
+
+        def combs(i, target, comb):
+            if target == 0:
+                res.append(comb.copy())
+            for j in range(i, lc):
+                val = candidates[j]
+                if val > target:
+                    break
+                comb.append(val)
+                combs(j, target - val, comb)
+                comb.pop()
+
+        combs(0, target, [])
+
+        return res
+
+
+class Solution1:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
         lc = len(candidates)
 
@@ -14,7 +36,7 @@ class Solution:
                     cmb(comb, nsm, i)
                     comb.pop()
                 elif nsm == target:
-                    ans.append(comb + candidates[i:i + 1])
+                    ans.append(comb + candidates[i : i + 1])
 
         cmb([], 0, 0)
         return ans
@@ -43,18 +65,22 @@ class Solution2:
 def test():
     sol = Solution()
 
-    print('Test 1 ... ', end='')
+    print("Test 1 ... ", end="")
     assert sol.combinationSum(candidates=[2, 3, 6, 7], target=7) == [[2, 2, 3], [7]]
-    print('ok')
+    print("OK")
 
-    print('Test 2 ... ', end='')
-    assert sol.combinationSum(candidates=[2, 3, 5], target=8) == [[2, 2, 2, 2], [2, 3, 3], [3, 5]]
-    print('ok')
+    print("Test 2 ... ", end="")
+    assert sol.combinationSum(candidates=[2, 3, 5], target=8) == [
+        [2, 2, 2, 2],
+        [2, 3, 3],
+        [3, 5],
+    ]
+    print("OK")
 
-    print('Test 3 ... ', end='')
+    print("Test 3 ... ", end="")
     assert sol.combinationSum(candidates=[2], target=1) == []
-    print('ok')
+    print("OK")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test()
