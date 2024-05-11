93fe75d23bdc4d64e75c5abd1a3c9387b17c30cf
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-05-11 18:47:48 +0300

    modified 1 daily solution

--- leetcode_solutions/p857_minimum_cost_to_hire_k_workers.py
+++ leetcode_solutions/p857_minimum_cost_to_hire_k_workers.py
@@ -1,13 +1,51 @@
-from heapq import heappush, heappop
+from heapq import heappush, heappop, heappushpop
 from fractions import Fraction
 
 
-class Solution(object):
+class Solution:
     def mincostToHireWorkers(self, quality, wage, k):
-        workers = sorted((Fraction(w, q), q, w)
-                         for q, w in zip(quality, wage))
+        workers = sorted((w / q, q) for q, w in zip(quality, wage))
+        pool, sumq = [], 0
 
-        ans = float('inf')
+        for i in range(k):
+            heappush(pool, -workers[i][1])
+            sumq += workers[i][1]
+
+        ans = workers[k - 1][0] * sumq
+
+        for i in range(k, len(wage)):
+            ratio, q = workers[i]
+            sumq += q + heappushpop(pool, -q)
+            ans = min(ans, ratio * sumq)
+
+        return ans
+
+
+class Solution2:
+    def mincostToHireWorkers(self, quality, wage, k):
+        workers = sorted((w / q, q, w) for q, w in zip(quality, wage))
+
+        ans = float("inf")
+        pool = []
+        sumq = 0
+        for ratio, q, w in workers:
+            heappush(pool, -q)
+            sumq += q
+
+            if len(pool) > k:
+                sumq += heappop(pool)
+
+            if len(pool) == k:
+                ans = min(ans, ratio * sumq)
+
+        return float(ans)
+
+
+class Solution3:
+    def mincostToHireWorkers(self, quality, wage, k):
+        workers = sorted((Fraction(w, q), q, w) for q, w in zip(quality, wage))
+
+        ans = float("inf")
         pool = []
         sumq = 0
         for ratio, q, w in workers:
@@ -26,14 +64,25 @@ class Solution(object):
 def test():
     sol = Solution()
 
-    print('Test 1... ', end='')
-    assert round(sol.mincostToHireWorkers(quality=[10, 20, 5], wage=[70, 50, 30], k=2), 5) == 105.00000
-    print('OK')
+    print("Test 1... ", end="")
+    assert (
+        round(sol.mincostToHireWorkers(quality=[10, 20, 5], wage=[70, 50, 30], k=2), 5)
+        == 105.00000
+    )
+    print("OK")
 
-    print('Test 2... ', end='')
-    assert round(sol.mincostToHireWorkers(quality=[3, 1, 10, 10, 1], wage=[4, 8, 2, 2, 7], k=3), 5) == 30.66667
-    print('OK')
+    print("Test 2... ", end="")
+    assert (
+        round(
+            sol.mincostToHireWorkers(
+                quality=[3, 1, 10, 10, 1], wage=[4, 8, 2, 2, 7], k=3
+            ),
+            5,
+        )
+        == 30.66667
+    )
+    print("OK")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test()
