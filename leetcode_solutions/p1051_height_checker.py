35a7d952d63e4bbafab990f5db15f1343a14ac1b
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-06-10 11:17:50 +0300

    modified 1 daily solution

--- leetcode_solutions/p1051_height_checker.py
+++ leetcode_solutions/p1051_height_checker.py
@@ -1,26 +1,37 @@
+import unittest
 from typing import List
 
 
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
+        return sum(a != b for a, b in zip(heights, sorted(heights)))
+
+
+class Solution2:
+    def heightChecker(self, heights: List[int]) -> int:
         return sum(1 for a, b in zip(heights, sorted(heights)) if a != b)
 
 
-def test():
-    sol = Solution()
+class TestSolution(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.sol = Solution()
 
-    print("Test 1... ", end="")
-    assert sol.heightChecker(heights=[1, 1, 4, 2, 1, 3]) == 3
-    print("OK")
+    def test_height_checker1(self):
+        print("Test heightChecker 1 ... ", end="")
+        self.assertEqual(self.sol.heightChecker(heights=[1, 1, 4, 2, 1, 3]), 3)
+        print("OK")
 
-    print("Test 2... ", end="")
-    assert sol.heightChecker(heights=[5, 1, 2, 3, 4]) == 5
-    print("OK")
+    def test_height_checker2(self):
+        print("Test heightChecker 2 ... ", end="")
+        self.assertEqual(self.sol.heightChecker(heights=[5, 1, 2, 3, 4]), 5)
+        print("OK")
 
-    print("Test 3... ", end="")
-    assert sol.heightChecker(heights=[1, 2, 3, 4, 5]) == 0
-    print("OK")
+    def test_height_checker3(self):
+        print("Test heightChecker 3 ... ", end="")
+        self.assertEqual(self.sol.heightChecker(heights=[1, 2, 3, 4, 5]), 0)
+        print("OK")
 
 
 if __name__ == "__main__":
-    test()
+    unittest.main()
