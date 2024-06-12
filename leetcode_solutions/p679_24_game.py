8940583a1df3eeca52c45aa800214e0d4c86422b
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-06-12 14:43:28 +0300

    added 1 daily solution

---
+++ leetcode_solutions/p679_24_game.py
@@ -0,0 +1,45 @@
+import unittest
+from functools import reduce
+from itertools import permutations
+from math import inf, isclose
+from operator import truediv, add, sub, mul
+from typing import List
+
+div = lambda x, y: reduce(truediv, (x, y)) if y else inf
+ops = (add, sub, mul, div)
+
+
+class Solution:
+    def judgePoint24(self, cards: List[int]) -> bool:
+        def dfs(c: list) -> bool:
+            if len(c) < 2:
+                return isclose(c[0], 24)
+
+            for p in set(permutations(c)):
+                for num in {reduce(op, p[:2]) for op in ops}:
+                    if dfs([num] + list(p[2:])):
+                        return True
+
+            return False
+
+        return dfs(cards)
+
+
+class TestSolution(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.sol = Solution()
+
+    def test_judge_point_24_1(self):
+        print("Test judgePoint24 1 ... ", end="")
+        self.assertTrue(self.sol.judgePoint24([4, 1, 8, 7]))
+        print("OK")
+
+    def test_judge_point_24_2(self):
+        print("Test judgePoint24 2 ... ", end="")
+        self.assertFalse(self.sol.judgePoint24([1, 2, 1, 2]))
+        print("OK")
+
+
+if __name__ == "__main__":
+    unittest.main()
