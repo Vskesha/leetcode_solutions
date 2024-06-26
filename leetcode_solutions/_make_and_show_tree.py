59d723d18917fba45c4a4a12e1a6a7c469a177db
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-06-27 02:25:44 +0300

    added 2 files

---
+++ leetcode_solutions/_make_and_show_tree.py
@@ -0,0 +1,104 @@
+from collections import deque
+from typing import Optional, List
+import matplotlib.pyplot as plt
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class TreeCreator:
+    def get_text_for_graph_editor(self, root: TreeNode) -> str:
+        relations = []
+
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            if node.left:
+                queue.append(node.left)
+                relations.append(f"{node.val} {node.left.val}")
+            if node.right:
+                queue.append(node.right)
+                relations.append(f"{node.val} {node.right.val}")
+
+        return "\n".join(relations)
+
+    def list_to_tree(self, arr: List[int]) -> Optional[TreeNode]:
+        if not arr:
+            return None
+
+        vals = iter(arr)
+        root = TreeNode(next(vals))
+        que = deque([root])
+
+        while que:
+            node = que.popleft()
+            val = next(vals, None)
+            if val is not None:
+                node.left = TreeNode(val)
+                que.append(node.left)
+
+            val = next(vals, None)
+            if val is not None:
+                node.right = TreeNode(val)
+                que.append(node.right)
+
+        return root
+
+    def plot_tree(self, node, x=0.0, y=0.0, branch_length=2.0):
+        if node is not None:
+            plt.text(x, y, str(node.val), fontsize=20, ha='center', va='center')
+            # plt.text(x, y, "◯", fontsize=50, ha='center', va='center')
+            # ax = plt.gca()
+            # circle = plt.Circle((x, y), 0.05, color="g")
+            # ax.add_patch(circle)
+            if node.left is not None:
+                plt.plot([x, x-branch_length], [y, y-1], 'k-', marker='o', markersize=50, markerfacecolor="w")
+                self.plot_tree(node.left, x-branch_length, y-1, branch_length/2)
+            if node.right is not None:
+                plt.plot([x, x+branch_length], [y, y-1], 'k-', marker='o', markersize=50, markerfacecolor="w")
+                self.plot_tree(node.right, x+branch_length, y-1, branch_length/2)
+
+    def show_tree(self, arr: List[int]) -> None:
+        root = self.list_to_tree(arr)
+        plt.figure(figsize=(15, 10))
+        self.plot_tree(root)
+        plt.axis('off')
+        plt.subplots_adjust(left=0.0, right=1.0, top=1.0, bottom=0.0)
+        plt.ylim(-6.5, 0.5)
+        plt.show()
+
+
+if __name__ == "__main__":
+    null = None
+    arr = [
+            1,
+            null,
+            15,
+            14,
+            17,
+            7,
+            null,
+            null,
+            null,
+            2,
+            12,
+            null,
+            3,
+            9,
+            null,
+            null,
+            null,
+            null,
+            11,
+        ]
+    tc = TreeCreator()
+    tc.show_tree(arr)
+    # print(tc.get_text_for_graph_editor(root))
+
+
+
+
