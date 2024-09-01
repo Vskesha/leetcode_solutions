fea5b3baf0a08aa4cef28bbde6af1a2d41470f9e
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-09-01 10:12:40 +0300

    added 3270, 3271, 3272, 3273 solutions of biweekly contest 138

---
+++ leetcode_solutions/p3272_find_the_count_of_good_integers.py
@@ -0,0 +1,48 @@
+import unittest
+from collections import Counter
+from math import factorial
+
+from leetcode_solutions._test_meta import TestMeta
+
+
+class Solution:
+    def countGoodIntegers(self, n: int, k: int) -> int:
+        n2 = (n + 1) // 2
+        st = n % 2
+        seen = set()
+        ans = 0
+
+        for fh in range(10 ** (n2 - 1), 10**n2):
+            sfh = str(fh)
+            sn = sfh + sfh[::-1][st:]
+            if int(sn) % k == 0:
+                key = "".join(sorted(sn))
+                if key in seen:
+                    continue
+                seen.add(key)
+                cnt = Counter(sn)
+                combs = factorial(n - 1) * (n - cnt["0"])
+                for val in cnt.values():
+                    combs //= factorial(val)
+                ans += combs
+
+        return ans
+
+
+class TestSolution(unittest.TestCase, metaclass=TestMeta):
+    test_cases = [
+        {
+            "class": Solution,
+            "class_methods": ["countGoodIntegers"] * 3,
+            "kwargs": [
+                dict(n = 3, k = 5),
+                dict(n = 1, k = 4),
+                dict(n = 5, k = 6),
+            ],
+            "expected": [27, 2, 2468],
+        },
+    ]
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ leetcode_solutions/p3273_minimum_amount_of_damage_dealt_to_bob.py
@@ -0,0 +1,39 @@
+import unittest
+from typing import List
+
+from leetcode_solutions._test_meta import TestMeta
+
+
+class Solution:
+    def minDamage(self, power: int, damage: List[int], health: List[int]) -> int:
+        n = len(damage)
+
+        times = [(h - 1) // power + 1 for h in health]
+        dpt = [(d / t, d, t) for d, t in zip(damage, times)]
+        dpt.sort(reverse=True)
+
+        td = sum(damage)
+        ans = 0
+
+        for _, d, t in dpt:
+            ans += td * t
+            td -= d
+
+        return ans
+class TestSolution(unittest.TestCase, metaclass=TestMeta):
+    test_cases = [
+        {
+            "class": Solution,
+            "class_methods": ["minDamage"] * 3,
+            "kwargs": [
+                dict(power = 4, damage = [1,2,3,4], health = [4,5,6,8]),
+                dict(power = 1, damage = [1,1,1,1], health = [1,2,3,4]),
+                dict(power = 8, damage = [40], health = [59]),
+            ],
+            "expected": [39, 20, 320],
+        },
+    ]
+
+
+if __name__ == '__main__':
+    unittest.main()
