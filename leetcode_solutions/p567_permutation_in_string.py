c136b73894880c4ad150d7f9729ba60335a312f1
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-10-05 17:12:00 +0300

    modified 567 solution of daily problems

--- leetcode_solutions/p567_permutation_in_string.py
+++ leetcode_solutions/p567_permutation_in_string.py
@@ -4,6 +4,26 @@ from collections import Counter
 
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        rem = Counter(s1)
+        st = 0
+
+        for i, ch in enumerate(s2):
+            rem[ch] -= 1
+            if rem[ch] == 0:
+                del rem[ch]
+                if not rem:
+                    return True
+            elif rem[ch] == -1:
+                while rem[ch] == -1:
+                    rem[s2[st]] += 1
+                    st += 1
+                del rem[ch]
+
+        return False
+
+
+class Solution2:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
 
         ws = len(s1) - 1
         cnt1 = Counter(s1)
@@ -14,6 +34,30 @@ class Solution:
             if cnt1 == cnt2:
                 return True
             cnt2[c1] -= 1
+
+        return False
+
+
+class Solution3:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        ls1, ls2 = len(s1), len(s2)
+        if ls1 > ls2:
+            return False
+        diff = Counter(s1)
+        for ch in s2[: ls1 - 1]:
+            diff[ch] -= 1
+            if not diff[ch]:
+                del diff[ch]
+
+        for sch, ech in zip(s2, s2[ls1 - 1 :]):
+            diff[ech] -= 1
+            if not diff[ech]:
+                del diff[ech]
+            if not diff:
+                return True
+            diff[sch] += 1
+            if not diff[sch]:
+                del diff[sch]
 
         return False
 
@@ -34,5 +78,5 @@ class TestSolution(unittest.TestCase):
         print("OK")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
