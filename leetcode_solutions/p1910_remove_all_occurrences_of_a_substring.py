4e1cdb303f6e84229283a309d3495ec662eca186
vskesha <vskesha@gmail.com>
2025-02-11 23:04:23 +0200

    added 1910 solution of daily tasks

---
+++ leetcode_solutions/p1910_remove_all_occurrences_of_a_substring.py
@@ -0,0 +1,30 @@
+import unittest
+
+from leetcode_solutions._test_meta import TestMeta
+
+
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        lp = len(part)
+        i = s.find(part)
+        while i != -1:
+            s = s[:i] + s[i + lp:]
+            i = s.find(part)
+        return s
+
+class TestSolution(unittest.TestCase, metaclass=TestMeta):
+    test_cases = [
+        {
+            "class": Solution,
+            "class_methods": ["removeOccurrences"] * 2,
+            "kwargs": [
+                dict(s = "daabcbaabcbc", part = "abc"),
+                dict(s = "axxxxyyyyb", part = "xy"),
+            ],
+            "expected": ["dab", "ab"],
+        },
+    ]
+
+
+if __name__ == "__main__":
+    unittest.main()
