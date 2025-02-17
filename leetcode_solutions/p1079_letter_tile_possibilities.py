4ffad37fb2321407f53c29059015c36565e67c16
vskesha <vskesha@gmail.com>
2025-02-17 20:38:33 +0200

    added 1718 and modified 1079 solutions of daily tasks

--- leetcode_solutions/p1079_letter_tile_possibilities.py
+++ leetcode_solutions/p1079_letter_tile_possibilities.py
@@ -1,7 +1,38 @@
+import unittest
+from collections import Counter
 from itertools import permutations
+
+from leetcode_solutions._test_meta import TestMeta
 
 
 class Solution:
+    fact = [1] * 8
+    for i in range(2, 8):
+        fact[i] = fact[i - 1] * i
+
+    def numTilePossibilities(self, tiles: str) -> int:
+        cnt = list(Counter(tiles).values())
+        lc = len(cnt)
+
+        def dfs(i, taken, fact) -> int:
+            if i == lc:
+                ans = fact[sum(taken)]
+                for v in taken:
+                    ans //= fact[v]
+                return ans
+
+            ans = 0
+            for v in range(cnt[i] + 1):
+                taken.append(v)
+                ans += dfs(i + 1, taken, fact)
+                taken.pop()
+
+            return ans
+
+        return dfs(0, [], self.fact) - 1
+
+
+class Solution1:
     def numTilePossibilities(self, tiles: str) -> int:
         res = set()
 
@@ -25,21 +56,20 @@ class Solution2:
         return len(result)
 
 
-def test():
-    sol = Solution()
+class TestSolution(unittest.TestCase, metaclass=TestMeta):
+    test_cases = [
+        {
+            "class": Solution,
+            "class_methods": ["numTilePossibilities"] * 3,
+            "kwargs": [
+                dict(tiles="AAB"),
+                dict(tiles="AAABBC"),
+                dict(tiles="V"),
+            ],
+            "expected": [8, 188, 1],
+        },
+    ]
 
-    print('Test 1 ... ', end='')
-    assert sol.numTilePossibilities(tiles="AAB") == 8
-    print('OK')
 
-    print('Test 2 ... ', end='')
-    assert sol.numTilePossibilities(tiles="AAABBC") == 188
-    print('OK')
-
-    print('Test 3 ... ', end='')
-    assert sol.numTilePossibilities(tiles="V") == 1
-    print('OK')
-
-
-if __name__ == '__main__':
-    test()
+if __name__ == "__main__":
+    unittest.main()
---
+++ leetcode_solutions/p1718_construct_the_lexicographically_largest_valid_sequence.py
@@ -0,0 +1,134 @@
+import unittest
+from typing import List
+
+from sortedcontainers import SortedList
+
+from leetcode_solutions._test_meta import TestMeta
+
+
+class Solution:
+    def constructDistancedSequence(self, n: int) -> List[int]:
+        seen = [False] * (n + 1)
+        lr = 2 * n - 1
+        res = [0] * lr
+
+        def backtrack(i: int) -> bool:
+            if i == lr:
+                return True
+            if res[i]:
+                return backtrack(i + 1)
+
+            for j in range(n, 0, -1):
+                if not seen[j]:
+                    seen[j] = True
+                    res[i] = j
+                    if j == 1:
+                        if backtrack(i + 1):
+                            return True
+                    elif i + j < lr and res[i + j] == 0:
+                        res[i + j] = j
+                        if backtrack(i + 1):
+                            return True
+                        res[i + j] = 0
+                    res[i] = 0
+                    seen[j] = False
+
+            return False
+
+        backtrack(0)
+        return res
+
+
+class Solution1:
+    def constructDistancedSequence(self, n: int) -> List[int]:
+        seen = [False] * (n + 1)
+        lr = 2 * n - 1
+        res = [0] * lr
+
+        def backtrack(i: int) -> bool:
+            while i < lr and res[i]:
+                i += 1
+            if i == lr:
+                return True
+            for j in range(n, 1, -1):
+                if seen[j] or i + j >= lr or res[i + j]:
+                    continue
+                seen[j] = True
+                res[i] = res[i + j] = j
+                if backtrack(i + 1):
+                    return True
+                res[i] = res[i + j] = 0
+                seen[j] = False
+
+            if not seen[1]:
+                res[i] = 1
+                seen[1] = True
+                if backtrack(i + 1):
+                    return True
+                res[i] = 0
+                seen[1] = False
+
+            return False
+
+        backtrack(0)
+        return res
+
+class Solution2:
+    def constructDistancedSequence(self, n: int) -> List[int]:
+        nums = SortedList(range(1, n + 1))
+        lr = 2 * n - 1
+        res = [0] * lr
+
+        def dfs(nums, mi):
+            if not nums:
+                return True
+
+            while res[mi]:
+                mi += 1
+
+            desc = list(reversed(nums))
+            for n in desc:
+                if n == 1:
+                    res[mi] = n
+                    nums.remove(n)
+                    if dfs(nums, mi):
+                        return True
+                    res[mi] = 0
+                    nums.add(n)
+                elif (mi + n < lr and
+                      res[mi + n] == 0):
+                    res[mi] = res[mi + n] = n
+                    nums.remove(n)
+                    if dfs(nums, mi + 1):
+                        return True
+                    res[mi] = res[mi + n] = 0
+                    nums.add(n)
+
+            return False
+
+        dfs(nums, 0)
+        return res
+
+
+class TestSolution(unittest.TestCase, metaclass=TestMeta):
+    test_cases = [
+        {
+            "class": Solution,
+            "class_methods": ["constructDistancedSequence"] * 3,
+            "kwargs": [
+                dict(n=3),
+                dict(n=5),
+                dict(n=7),
+            ],
+            "expected": [
+                [3, 1, 2, 3, 2],
+                [5, 3, 1, 4, 3, 5, 2, 4, 2],
+                [7, 5, 3, 6, 4, 3, 5, 7, 4, 6, 2, 1, 2]
+            ],
+            "assert_methods": ["assertListEqual"] * 3,
+        },
+    ]
+
+
+if __name__ == "__main__":
+    unittest.main()
