8994e4670920d50e625ab1dd1bd1631b60e41dd5
vskesha <vskesha@gmail.com>
2025-02-28 14:48:29 +0200

    modified names of files (all problem numbers are 4 digit)

---
+++ leetcode_solutions/__rename_files_to_have_four_digits_problem_number.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+
+def rename_not_four_digit():
+    current_folder = Path(__file__).parent
+
+    for item in current_folder.iterdir():
+        if item.is_file() and item.name.startswith("p"):
+            name = item.name
+            i = name.index("_")
+            if i == 5:
+                continue
+            print(name, end="  -->  ")
+            new_name = "p" + "0" * (5 - i) + name[1:]
+            print(new_name)
+            item.rename(new_name)
+
+
+if __name__ == '__main__':
+    rename_not_four_digit()
