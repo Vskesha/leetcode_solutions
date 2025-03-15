d2afb050293cb25ec857b31421e725c6f6d5e847
vskesha <vskesha@gmail.com>
2025-03-15 18:43:29 +0200

    added 2226 and 2560 solutions of daily tasks

---
+++ leetcode_solutions/p2560_house_robber_iv.py
@@ -0,0 +1,58 @@
+import unittest
+from typing import List
+
+from leetcode_solutions._test_meta import TestMeta
+
+
+class Solution:
+    def minCapability(self, nums: List[int], k: int) -> int:
+
+        def can_rob_k_houses(capability):
+            cnt = 0
+            taken = False
+            for n in nums:
+                if taken:
+                    taken = False
+                elif n <= capability:
+                    cnt += 1
+                    taken = True
+
+            return cnt >= k
+
+            # or...
+
+            # i = cnt = 0
+            # while i < len(nums):
+            #     if nums[i] <= capability:
+            #         cnt += 1
+            #         i += 2
+            #     else:
+            #         i += 1
+            # return cnt >= k
+
+        l, r = 1, max(nums)
+        while l < r:
+            m = (l + r) // 2
+            if can_rob_k_houses(m):
+                r = m
+            else:
+                l = m + 1
+        return r
+
+
+class TestSolution(unittest.TestCase, metaclass=TestMeta):
+    test_cases = [
+        {
+            "class": Solution,
+            "class_methods": ["minCapability"] * 2,
+            "kwargs": [
+                dict(nums = [2,3,5,9], k = 2),
+                dict(nums = [2,7,9,3,1], k = 2),
+            ],
+            "expected": [5, 2],
+        },
+    ]
+
+
+if __name__ == "__main__":
+    unittest.main()
