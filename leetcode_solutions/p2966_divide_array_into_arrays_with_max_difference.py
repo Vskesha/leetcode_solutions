383e1fc0de275293a8aaaa9eb89bd1e46d70a037
Vasyl Boliukh <vskesha@gmail.com>
2025-06-18 08:59:07 +0300

    modified 2966 daily task

--- leetcode_solutions/p2966_divide_array_into_arrays_with_max_difference.py
+++ leetcode_solutions/p2966_divide_array_into_arrays_with_max_difference.py
@@ -6,12 +6,24 @@ from leetcode_solutions._test_meta import TestMeta
 
 class Solution:
     def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
+        nums = sorted(nums)
+        ln = len(nums)
+
+        for i in range(0, ln, 3):
+            if nums[i + 2] - nums[i] > k:
+                return []
+
+        return [nums[i : i + 3] for i in range(0, ln, 3)]
+
+
+class Solution2:
+    def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
         nums.sort()
         ln = len(nums)
         ans = []
         for i in range(0, ln, 3):
             if nums[i] + k >= nums[i + 2]:
-                ans.append(nums[i: i + 3])
+                ans.append(nums[i : i + 3])
             else:
                 return []
         return ans
@@ -25,12 +37,25 @@ class TestSolution(unittest.TestCase, metaclass=TestMeta):
             "kwargs": [
                 dict(nums=[1, 3, 4, 8, 7, 9, 3, 5, 1], k=2),
                 dict(nums=[2, 4, 2, 2, 5, 2], k=2),
-                dict(nums=[4, 2, 9, 8, 2, 12, 7, 12, 10, 5, 8, 5, 5, 7, 9, 2, 5, 11], k=14)
+                dict(
+                    nums=[4, 2, 9, 8, 2, 12, 7, 12, 10, 5, 8, 5, 5, 7, 9, 2, 5, 11],
+                    k=14,
+                ),
             ],
             "expected": [
                 ([[1, 1, 3], [3, 4, 5], [7, 8, 9]], 2),
                 ([], 2),
-                ([[2, 2, 12], [4, 8, 5], [5, 9, 7], [7, 8, 5], [5, 9, 10], [11, 12, 2]], 14),
+                (
+                    [
+                        [2, 2, 12],
+                        [4, 8, 5],
+                        [5, 9, 7],
+                        [7, 8, 5],
+                        [5, 9, 10],
+                        [11, 12, 2],
+                    ],
+                    14,
+                ),
             ],
             "assert_methods": ["assertMaxDiffIsLessEqual"] * 3,
         },
