6a8f09e519fe6bf6a930bc91b3af13075cc373e7
Vasyl Boliukh <vskesha@gmail.com>
2026-03-13 19:01:24 +0200

    modified 3296 solution of daily task

--- leetcode_solutions/p3296_minimum_number_of_seconds_to_make_mountain_height_zero.py
+++ leetcode_solutions/p3296_minimum_number_of_seconds_to_make_mountain_height_zero.py
@@ -9,12 +9,51 @@ class Solution:
     def minNumberOfSeconds(
         self, mountainHeight: int, workerTimes: List[int]
     ) -> int:
+        right = workerTimes[0] * mountainHeight * (mountainHeight + 1) // 2
+        left = 1
+
+        while left < right:
+            mid = (left + right) // 2
+            if (
+                sum(
+                    int((2 * mid / wt + 0.25) ** 0.5 - 0.5)
+                    for wt in workerTimes
+                )
+                >= mountainHeight
+            ):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
+
+
+class Solution2:
+    def minNumberOfSeconds(
+        self, mountainHeight: int, workerTimes: List[int]
+    ) -> int:
         heap = [(t, t, 2) for t in workerTimes]
         heapify(heap)
         for _ in range(mountainHeight):
             tt, t, r = heappop(heap)
             heappush(heap, (tt + t * r, t, r + 1))
         return tt
+
+
+class Solution3:
+    def minNumberOfSeconds(
+        self, mountainHeight: int, workerTimes: List[int]
+    ) -> int:
+        heap = []
+
+        for i, wt in enumerate(workerTimes):
+            heappush(heap, (wt, i, 2))
+
+        for _ in range(mountainHeight - 1):
+            wt, i, k = heappop(heap)
+            heappush(heap, (wt + workerTimes[i] * k, i, k + 1))
+
+        return heap[0][0]
 
 
 class TestSolution(unittest.TestCase, metaclass=TestMeta):
