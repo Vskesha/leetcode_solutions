0a52ec11ab3de543855ec3213d389a18a5f35293
Vasyl Boliukh <vskesha@gmail.com>
2026-03-17 23:56:52 +0200

    Modify 1727 solution of daily tasks

--- leetcode_solutions/p1727_largest_submatrix_with_rearrangements.py
+++ leetcode_solutions/p1727_largest_submatrix_with_rearrangements.py
@@ -1,7 +1,41 @@
+import unittest
 from typing import List
+
+from leetcode_solutions._test_meta import TestMeta
 
 
 class Solution:
+    def largestSubmatrix(self, matrix: List[List[int]]) -> int:
+        n = len(matrix[0])
+        crow = []
+        ans = 0
+
+        for row in matrix:
+            seen = [False] * n
+            nrow = []
+            width = 0
+
+            for height, j in crow:
+                seen[j] = True
+                if not row[j]:
+                    continue
+                height += 1
+                width += 1
+                nrow.append((height, j))
+                ans = max(ans, height * width)
+
+            for j in range(n):
+                if not seen[j] and row[j]:
+                    width += 1
+                    nrow.append((1, j))
+                    ans = max(ans, width)
+
+            crow = nrow
+
+        return ans
+
+
+class Solution2:
     def largestSubmatrix(self, matrix: List[List[int]]) -> int:
         prev = matrix[0]
         m = len(prev)
@@ -23,21 +57,20 @@ class Solution:
         return ans
 
 
-def test():
-    sol = Solution()
-
-    print("Test 1... ", end="")
-    assert sol.largestSubmatrix(matrix=[[0, 0, 1], [1, 1, 1], [1, 0, 1]]) == 4
-    print("OK")
-
-    print("Test 2... ", end="")
-    assert sol.largestSubmatrix(matrix=[[1, 0, 1, 0, 1]]) == 3
-    print("OK")
-
-    print("Test 3... ", end="")
-    assert sol.largestSubmatrix(matrix=[[1, 1, 0], [1, 0, 1]]) == 2
-    print("OK")
+class TestSolution(unittest.TestCase, metaclass=TestMeta):
+    test_cases = [
+        {
+            "class": Solution,
+            "class_methods": ["largestSubmatrix"] * 3,
+            "kwargs": [
+                dict(matrix = [[0,0,1],[1,1,1],[1,0,1]]),
+                dict(matrix = [[1,0,1,0,1]]),
+                dict(matrix = [[1,1,0],[1,0,1]]),
+            ],
+            "expected": [4, 3, 2],
+        },
+    ]
 
 
 if __name__ == "__main__":
-    test()
+    unittest.main()
